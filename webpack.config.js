81bf35ef5bc1132f052644f354a7f5961f75cf86
Bruno Caceres <brunocaceres@gmail.com>
2018-07-29 16:28:28 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: path.join(__dirname, 'src/index.jsx'),
+   output: {
+       path: path.join(__dirname, 'dist'),
+       filename: 'bundle.js'
+   },
+   resolve: {
+   extensions: [".js",".jsx"]
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       filename: 'index.html',
+       template: path.join(__dirname, 'src/index.html')
+     })
+   ],
+   module: {
+     rules: [
+       {
+         test: /.jsx?$/,
+         exclude: /node_modules/,
+         include: path.join(__dirname, 'src'),
+         use: [
+           {
+             loader: 'babel-loader',
+             options: {
+               presets: ['es2015', 'react']
+             }
+           }
+         ]
+       }
+     ]
+   },
+   devServer: {
+     host: "0.0.0.0",
+     port: "80",
+     publicPath: "/",
+     contentBase: "./dist"
+   }
+};
