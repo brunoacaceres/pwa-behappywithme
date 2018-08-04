00d22d84ddd9cdaccdde550f78ae10bbc66342a5
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2018-08-04 16:27:40 -0400

    Campo Nome Editável.

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function Label(props)  {
+  const estilo = {
+    color: props.valorInvalido ?  '#d50000' : '#444444'
+  };
   return (
-    <label htmlFor={props.htmlFor}>{props.texto}</label>
+    //<label htmlFor={props.htmlFor}>{props.texto}</label>
+    <label style={estilo}
+    htmlFor={props.htmlFor}>
+    {props.texto}
+    </label>
   );
 }
--- src/components/NovoUsuario/index.jsx
+++ src/components/NovoUsuario/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import Label from '../Label'
+import Input from '../Input'
 
 class NovoUsuario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nomeInvalido: true
+    };
+  }
   render() {
     return(
       <div className="center">
-        <form>
-            <Label htmlFor="Nome" texto="Quem é você?" />
+        <form className="pure-form pure-form-stacked">
+            <Label
+              htmlFor="Nome"
+              texto="Quem é você?"
+              valorInvalido={this.state.nomeInvalido}
+            />
+            <Input
+              id="nome"
+              placeholder="Digite seu nome"
+              //maxLenght="40"
+              readOnly={false}
+              valorInvalido={this.state.nomeInvalido}
+              defaultValue="Bruno"
+            />
         </form>
       </div>
     );
