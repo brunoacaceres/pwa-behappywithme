f26576dc6a53965983a15acdfc8c1f34c506c687
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2018-08-04 16:50:46 -0400

    Componente campo nome.

--- src/components/NovoUsuario/index.jsx
+++ src/components/NovoUsuario/index.jsx
@@ -6,8 +6,21 @@ class NovoUsuario extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nomeInvalido: true
+      usuario: {
+        nome: ''
+      },
+      validacao: {
+        nomeInvalido: false
+      }
     };
+  }
+
+  atualizarNome(e) {
+    let usuario = this.state.usuario;
+    usuario.nome = e.target.value;
+    this.setState({
+        usuario: usuario
+    });
   }
   render() {
     return(
@@ -16,15 +29,17 @@ class NovoUsuario extends React.Component {
             <Label
               htmlFor="Nome"
               texto="Quem é você?"
-              valorInvalido={this.state.nomeInvalido}
+              valorInvalido={this.state.validacao.nomeInvalido}
             />
             <Input
               id="nome"
               placeholder="Digite seu nome"
               //maxLenght="40"
               readOnly={false}
-              valorInvalido={this.state.nomeInvalido}
-              defaultValue="Bruno"
+              valorInvalido={this.state.validacao.nomeInvalido}
+              //defaultValue="Bruno"
+              defaultValue={this.state.usuario.nome}
+              onChange={this.atualizarNome.bind(this)}
             />
         </form>
       </div>
